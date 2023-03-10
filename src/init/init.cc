187ddec4a0a2e0db567d05e5e238832041634e61
CVE Zeroday <cvezeroday5754@gmail.com>
2023-03-10 12:22:36 +0000

    Initial commit of init.cc

---
+++ src/init/init.cc
@@ -0,0 +1,17 @@
+/*******************************************/
+/* This source code is part of PulsarVoIP  */
+/*******************************************/
+/*     Copyrights (C) 2023 CVE_zeroday.    */
+/*          All rights reserved            */
+/*******************************************/
+/*   Created by CVE_zeroday on 09.03.2023  */
+/*               (T.Y.Kim)                 */
+/*******************************************/
+
+#include <thread>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+
+}
